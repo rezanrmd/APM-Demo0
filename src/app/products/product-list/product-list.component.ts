370f09e053957edca93827bfbee3ef68d673adae
Reza Nourmand <Reza.Nourmand@bighand.com>
2022-03-26 19:40:12 +0000

    first commit

---
+++ src/app/products/product-list/product-list.component.ts
@@ -0,0 +1,57 @@
+import { Component, OnInit, OnDestroy } from '@angular/core';
+import { Store } from '@ngrx/store';
+
+import { Subscription } from 'rxjs';
+
+import { Product } from '../product';
+import { ProductService } from '../product.service';
+
+@Component({
+  selector: 'pm-product-list',
+  templateUrl: './product-list.component.html',
+  styleUrls: ['./product-list.component.css']
+})
+export class ProductListComponent implements OnInit, OnDestroy {
+  pageTitle = 'Products';
+  errorMessage: string;
+
+  displayCode: boolean;
+
+  products: Product[];
+
+  // Used to highlight the selected product in the list
+  selectedProduct: Product | null;
+  sub: Subscription;
+
+  constructor(private productService: ProductService, private store: Store<any>) { }
+
+  ngOnInit(): void {
+    this.sub = this.productService.selectedProductChanges$.subscribe(
+      currentProduct => this.selectedProduct = currentProduct
+    );
+
+    this.productService.getProducts().subscribe({
+      next: (products: Product[]) => this.products = products,
+      error: err => this.errorMessage = err
+    });
+  }
+
+  ngOnDestroy(): void {
+    this.sub.unsubscribe();
+  }
+
+  checkChanged(): void {
+    this.store.dispatch({
+      type:'[Product] toggle product code' 
+    });
+  }
+
+  newProduct(): void {
+    this.productService.changeSelectedProduct(this.productService.newProduct());
+  }
+
+  productSelected(product: Product): void {
+    this.productService.changeSelectedProduct(product);
+  }
+
+}
